f61a3e154afc7d3a8947075beec35ebe079b93a9
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-27 05:55:49 -0700

    Pass school context to arm operations

--- server/controllers/armController.js
+++ server/controllers/armController.js
@@ -1,60 +1,27 @@
 const armService = require("../services/armService");
 
 const getArms = async (req, res, next) => {
-
     try {
-
-        const arms = await armService.getArms();
-
-        res.status(200).json({
-
-            success: true,
-
-            data: arms
-
-        });
-
-    }
-
-    catch (err) {
-
+        const arms = await armService.getArms(req.user.school_id);
+        res.status(200).json({ success: true, data: arms });
+    } catch (err) {
         next(err);
-
     }
-
 };
 
 const getArmsByClass = async (req, res, next) => {
-
     try {
-
         const arms = await armService.getArmsByClass(
-
-            req.params.classId
-
+            req.params.classId,
+            req.user.school_id
         );
-
-        res.status(200).json({
-
-            success: true,
-
-            data: arms
-
-        });
-
-    }
-
-    catch (err) {
-
+        res.status(200).json({ success: true, data: arms });
+    } catch (err) {
         next(err);
-
     }
-
 };
 
 module.exports = {
-
     getArms,
     getArmsByClass
-
 };
