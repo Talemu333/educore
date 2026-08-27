585f9a27aed92a6625580e01713af482d5240436
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-27 05:55:43 -0700

    Scope arm service by school

--- server/services/armService.js
+++ server/services/armService.js
@@ -1,60 +1,40 @@
 const ApiError = require("../utils/ApiError");
-
 const armModel = require("../models/armModel");
 const classModel = require("../models/classModel");
 
-const createArm = async (armData) => {
-
+const createArm = async (armData, schoolId) => {
     const classExists = await classModel.getClassById(
-        armData.class_id
+        armData.class_id,
+        schoolId
     );
 
     if (!classExists) {
-
-        throw new ApiError(
-            404,
-            "Selected class does not exist."
-        );
-
+        throw new ApiError(404, "Selected class does not exist in this school.");
     }
 
     const existing = await armModel.getArmByName(
-
         armData.class_id,
-
-        armData.arm_name
-
+        armData.arm_name,
+        schoolId
     );
 
     if (existing) {
-
-        throw new ApiError(
-            409,
-            "Arm already exists in this class."
-        );
-
+        throw new ApiError(409, "Arm already exists in this class.");
     }
 
-    return await armModel.createArm(armData);
-
+    return await armModel.createArm(armData, schoolId);
 };
 
-const getArms = async () => {
-
-    return await armModel.getArms();
-
+const getArms = async (schoolId) => {
+    return await armModel.getArms(schoolId);
 };
 
-const getArmsByClass = async (classId) => {
-
-    return await armModel.getArmsByClass(classId);
-
+const getArmsByClass = async (classId, schoolId) => {
+    return await armModel.getArmsByClass(classId, schoolId);
 };
 
 module.exports = {
-
     createArm,
     getArms,
     getArmsByClass
-
 };
