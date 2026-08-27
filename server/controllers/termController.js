698ea8f33de9353d24a0bc4027a64ffead0ef8e8
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-27 05:51:17 -0700

    Enforce school scope in term controller

--- server/controllers/termController.js
+++ server/controllers/termController.js
@@ -1,29 +1,18 @@
 const termService = require("../services/termService");
 
 const getTerms = async (req, res, next) => {
-
     try {
-
-        const terms = await termService.getTerms();
+        const terms = await termService.getTerms(req.user.school_id);
 
         res.json({
-
             success: true,
-
             data: terms
-
         });
-
     } catch (err) {
-
         next(err);
-
     }
-
 };
 
 module.exports = {
-
     getTerms
-
 };
