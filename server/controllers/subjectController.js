e76d013a12df5d1f5210cde2aa6313596ad56dcc
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-27 05:55:30 -0700

    Pass school context to subject operations

--- server/controllers/subjectController.js
+++ server/controllers/subjectController.js
@@ -1,60 +1,33 @@
 const subjectService = require("../services/subjectService");
-const { createSubjectSchema } = require("../validators/subjectValidator");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 const getSubjects = asyncHandler(async (req, res) => {
-
-    const subjects = await subjectService.getSubjects();
-
-    res.json({
-
-        success: true,
-
-        count: subjects.length,
-
-        data: subjects
-
-    });
-
+    const subjects = await subjectService.getSubjects(req.user.school_id);
+    res.json({ success: true, count: subjects.length, data: subjects });
 });
 
 const createSubject = asyncHandler(async (req, res) => {
-
-    const subject = await subjectService.createSubject(req.body);
-
+    const subject = await subjectService.createSubject(
+        req.body,
+        req.user.school_id
+    );
     res.status(201).json({
-
         success: true,
-
         message: "Subject created successfully.",
-
         data: subject
-
     });
-
 });
 
-const getSubjectsByClass =
-asyncHandler(async (req, res) => {
-
-    const subjects =
-        await subjectService.getSubjectsByClass(
-            req.params.classId
-        );
-
-    res.json({
-
-        success: true,
-
-        data: subjects
-
-    });
-
+const getSubjectsByClass = asyncHandler(async (req, res) => {
+    const subjects = await subjectService.getSubjectsByClass(
+        req.params.classId,
+        req.user.school_id
+    );
+    res.json({ success: true, data: subjects });
 });
 
 module.exports = {
     getSubjects,
     createSubject,
     getSubjectsByClass
-
 };
