a941b6f89a4a2079ec7af77c5d54ad5b081d6031
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-27 06:54:37 -0700

    Pass school context through timetable controller

--- server/controllers/timetableController.js
+++ server/controllers/timetableController.js
@@ -2,24 +2,12 @@ const timetableService = require("../services/timetableService");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 const createTimetable = asyncHandler(async (req, res) => {
-
-    const timetable =
-        await timetableService.createTimetable(req.body);
-
+    const timetable = await timetableService.createTimetable(req.body, req.user?.school_id);
     res.status(201).json({
-
         success: true,
-
         message: "Timetable created successfully.",
-
         data: timetable
-
     });
-
 });
 
-module.exports = {
-
-    createTimetable
-
-};
+module.exports={createTimetable};
