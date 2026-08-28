2c99b26f9cdaa966a4d8bc172d8edb5371683433
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-28 06:05:59 -0700

    Scope academic session seed to school

--- server/database/seeds/004_seed_academic_session.sql
+++ server/database/seeds/004_seed_academic_session.sql
@@ -1,8 +1,9 @@
 INSERT INTO academic_sessions
-(session_name, start_date, end_date, is_current)
+(school_id, session_name, start_date, end_date, is_current)
 
 VALUES
 (
+1,
 '2026/2027',
 '2026-09-14',
 '2027-07-30',
