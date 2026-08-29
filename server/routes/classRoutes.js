8e92a01542817a1cbe6c10cdfed0ba1d61d91f12
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-29 12:34:47 -0700

    fix class routes for school-scoped access

--- server/routes/classRoutes.js
+++ server/routes/classRoutes.js
@@ -5,9 +5,18 @@ const authorize = require("../middlewares/authorize");
 const ROLES = require("../constants/roles");
 const classController = require("../controllers/classController");
 
-router.get("/", classController.getClasses);
-router.get("/:id/arms", authenticate, classController.getClassArms);
-router.post("/", classController.createClass);
+router.get(
+    "/",
+    authenticate,
+    classController.getClasses
+);
+
+router.get(
+    "/:id/arms",
+    authenticate,
+    classController.getClassArms
+);
+
 router.post(
     "/",
     authenticate,
