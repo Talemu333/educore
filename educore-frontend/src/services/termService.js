088d50599f997829fde642c95075884faf2758e8
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-29 02:03:23 -0700

    Add academic term creation API service

--- educore-frontend/src/services/termService.js
+++ educore-frontend/src/services/termService.js
@@ -1,13 +1,11 @@
 import api from "@/api/axios";
 
 export const getTerms = async () => {
-
-    const response = await api.get(
-
-        "/terms"
-
-    );
-
+    const response = await api.get("/terms");
     return response.data.data;
+};
 
+export const createTerm = async (data) => {
+    const response = await api.post("/terms", data);
+    return response.data.data;
 };
