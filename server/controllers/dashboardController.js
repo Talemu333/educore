2bca8fc106b343e74669cf38b311d18cfc913de4
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-28 17:52:33 -0700

    Scope dashboard to authenticated school

--- server/controllers/dashboardController.js
+++ server/controllers/dashboardController.js
@@ -2,21 +2,21 @@ const asyncHandler = require("../middlewares/asyncHandler");
 const dashboardService = require("../services/dashboardService");
 
 const getDashboard = asyncHandler(async (req, res) => {
+    const schoolId = req.user?.school_id;
 
-    const dashboard = await dashboardService.getDashboard();
+    if (!schoolId) {
+        return res.status(403).json({
+            success: false,
+            message: "School context is required to load the dashboard."
+        });
+    }
+
+    const dashboard = await dashboardService.getDashboard(schoolId);
 
     res.status(200).json({
-
         success: true,
-
         data: dashboard
-
     });
-
 });
 
-module.exports = {
-
-    getDashboard
-
-};
+module.exports = { getDashboard };
