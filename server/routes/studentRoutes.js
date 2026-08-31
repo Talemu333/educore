fb76f7207ac6e76f6e9a105955a8bc50498c24c4
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-08-31 07:30:17 -0700

    Secure student parent endpoint with school authentication

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -4,7 +4,7 @@ const studentController = require("../controllers/studentController");
 const authenticate = require("../middlewares/authenticate");
 const authorize = require("../middlewares/authorize");
 const validate = require("../middlewares/validate");
-const {createStudentSchema,updateStudentSchema} = require("../validators/studentValidator");
+const { createStudentSchema, updateStudentSchema } = require("../validators/studentValidator");
 const ROLES = require("../constants/roles");
 
 router.post(
@@ -14,27 +14,21 @@ router.post(
     validate(createStudentSchema),
     studentController.createStudent
 );
-// router.get(
 
-//     "/search",
-
-//     authenticate,
-
-//     studentController.searchStudents
-
-// );
 router.get(
     "/",
     authenticate,
     authorize(ROLES.ADMIN),
     studentController.getAllStudents
 );
+
 router.get(
     "/:id",
     authenticate,
     authorize(ROLES.ADMIN),
     studentController.getStudentById
 );
+
 router.put(
     "/:id",
     authenticate,
@@ -42,19 +36,18 @@ router.put(
     validate(updateStudentSchema),
     studentController.updateStudent
 );
+
 router.patch(
-
     "/:id/deactivate",
-
     authenticate,
-
     authorize(ROLES.ADMIN),
-
     studentController.deactivateStudent
-
 );
+
 router.get(
     "/:id/parents",
+    authenticate,
+    authorize(ROLES.ADMIN),
     studentController.getStudentParents
 );
 
