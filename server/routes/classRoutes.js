180438cbab510dc83ea8aada440ee8765f7a1b62
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 08:45:41 -0700

    Allow Super Admin to manage selected school classes

--- server/routes/classRoutes.js
+++ server/routes/classRoutes.js
@@ -5,22 +5,12 @@ const authorize = require("../middlewares/authorize");
 const ROLES = require("../constants/roles");
 const classController = require("../controllers/classController");
 
-router.get(
-    "/",
-    authenticate,
-    classController.getClasses
-);
-
-router.get(
-    "/:id/arms",
-    authenticate,
-    classController.getClassArms
-);
-
+router.get("/", authenticate, classController.getClasses);
+router.get("/:id/arms", authenticate, classController.getClassArms);
 router.post(
     "/",
     authenticate,
-    authorize(ROLES.ADMIN),
+    authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN),
     classController.createClass
 );
 
