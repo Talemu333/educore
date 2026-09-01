6712722a73637a83d0f213d22eb3e20e8c0befd7
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 08:52:15 -0700

    Add password reset authentication routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
-
 const authController = require("../controllers/authController");
 
 router.post("/login", authController.login);
-
 router.post("/logout", authController.logout);
+router.post("/forgot-password", authController.requestPasswordReset);
+router.post("/reset-password", authController.resetPassword);
 
-router.get(
-    "/me",
-    authenticate,
-    authController.getCurrentUser
-);
-
-router.post(
-    "/change-password",
-    authenticate,
-    authController.changePassword
-);
+router.get("/me", authenticate, authController.getCurrentUser);
+router.post("/change-password", authenticate, authController.changePassword);
 
 module.exports = router;
