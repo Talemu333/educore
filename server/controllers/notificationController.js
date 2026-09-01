310a6d928df9781dfb51ad6a7f447ba1cc1697ad
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 01:02:01 -0700

    Scope notification reads to current school

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -1,59 +1,20 @@
-const notificationService =
-require("../services/notificationService");
+const notificationService = require("../services/notificationService");
+const asyncHandler = require("../middlewares/asyncHandler");
 
-const asyncHandler =
-require("../middlewares/asyncHandler");
+const getMyNotifications = asyncHandler(async (req, res) => {
+    const notifications = await notificationService.getUserNotifications(
+        req.user.id,
+        req.user.school_id
+    );
+    res.json({ success: true, data: notifications });
+});
 
-const getMyNotifications = asyncHandler(
+const markAsRead = asyncHandler(async (req, res) => {
+    const notification = await notificationService.markAsRead(
+        req.params.id,
+        req.user.school_id
+    );
+    res.json({ success: true, data: notification });
+});
 
-    async (req,res)=>{
-
-        const notifications =
-            await notificationService.getUserNotifications(
-
-                req.user.id
-
-            );
-
-        res.json({
-
-            success:true,
-
-            data:notifications
-
-        });
-
-    }
-
-);
-
-const markAsRead = asyncHandler(
-
-    async (req,res)=>{
-
-        const notification =
-            await notificationService.markAsRead(
-
-                req.params.id
-
-            );
-
-        res.json({
-
-            success:true,
-
-            data:notification
-
-        });
-
-    }
-
-);
-
-module.exports = {
-
-    getMyNotifications,
-
-    markAsRead
-
-};
+module.exports = { getMyNotifications, markAsRead };
