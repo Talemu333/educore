e73df98e4cdf54c82ea022a0031e780c707cddb3
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 08:45:47 -0700

    Allow Super Admin to manage selected school arms

--- server/routes/armRoutes.js
+++ server/routes/armRoutes.js
@@ -1,32 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
-
 const authenticate = require("../middlewares/authenticate");
 const authorize = require("../middlewares/authorize");
-
 const ROLES = require("../constants/roles");
-
 const armController = require("../controllers/armController");
 
-router.get(
-    "/",
-    authenticate,
-    authorize(ROLES.ADMIN),
-    armController.getArms
-);
-
-router.get(
-    "/class/:classId",
-    authenticate,
-    armController.getArmsByClass
-);
-
-router.post(
-    "/",
-    authenticate,
-    authorize(ROLES.ADMIN),
-    armController.createArm
-);
+router.get("/", authenticate, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.getArms);
+router.get("/class/:classId", authenticate, armController.getArmsByClass);
+router.post("/", authenticate, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.createArm);
 
 module.exports = router;
