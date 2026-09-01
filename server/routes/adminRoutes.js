9604a95abbcc80236793db0a4104fecab9d08885
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 08:45:14 -0700

    Allow Super Admin to manage selected school administrators

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,108 +1,16 @@
-const express =
-    require("express");
+const express = require("express");
 
-const router =
-    express.Router();
+const router = express.Router();
+const authenticate = require("../middlewares/authenticate");
+const authorize = require("../middlewares/authorize");
+const ROLE_NAMES = require("../config/roleNames");
+const adminController = require("../controllers/adminController");
 
+router.use(authenticate);
 
-const authenticate =
-    require("../middlewares/authenticate");
-
-const authorize =
-    require("../middlewares/authorize");
-
-const ROLE_NAMES =
-    require("../config/roleNames");
-
-const adminController =
-    require("../controllers/adminController");
-
-
-/*
-=========================================
-AUTHENTICATION
-=========================================
-*/
-
-router.use(
-    authenticate
-);
-
-
-/*
-=========================================
-GET ADMINISTRATORS
-=========================================
-*/
-
-router.get(
-
-    "/",
-
-    authorize(
-        ROLE_NAMES.ADMIN
-    ),
-
-    adminController.getAdmins
-
-);
-
-
-/*
-=========================================
-CREATE ADMINISTRATOR
-=========================================
-*/
-
-router.post(
-
-    "/",
-
-    authorize(
-        ROLE_NAMES.ADMIN
-    ),
-
-    adminController.createAdministrator
-
-);
-
-
-/*
-=========================================
-ACTIVATE ADMINISTRATOR
-=========================================
-*/
-
-router.patch(
-
-    "/:id/activate",
-
-    authorize(
-        ROLE_NAMES.ADMIN
-    ),
-
-    adminController.activateAdministrator
-
-);
-
-
-/*
-=========================================
-DEACTIVATE ADMINISTRATOR
-=========================================
-*/
-
-router.patch(
-
-    "/:id/deactivate",
-
-    authorize(
-        ROLE_NAMES.ADMIN
-    ),
-
-    adminController.deactivateAdministrator
-
-);
-
+router.get("/", authorize(ROLE_NAMES.ADMIN, ROLE_NAMES.SUPER_ADMIN), adminController.getAdmins);
+router.post("/", authorize(ROLE_NAMES.ADMIN, ROLE_NAMES.SUPER_ADMIN), adminController.createAdministrator);
+router.patch("/:id/activate", authorize(ROLE_NAMES.ADMIN, ROLE_NAMES.SUPER_ADMIN), adminController.activateAdministrator);
+router.patch("/:id/deactivate", authorize(ROLE_NAMES.ADMIN, ROLE_NAMES.SUPER_ADMIN), adminController.deactivateAdministrator);
 
 module.exports = router;
