c5da264c2d16c03dfcf6fd93c04f41556eb758c9
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 07:49:42 -0700

    Add Super Admin school routes

---
+++ server/routes/superAdminSchoolRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const authenticate = require("../middlewares/authenticate");
+const requireSuperAdmin = require("../middlewares/requireSuperAdmin");
+const controller = require("../controllers/superAdminSchoolController");
+
+const router = express.Router();
+
+router.use(authenticate, requireSuperAdmin);
+
+router.get("/", controller.getSchools);
+router.get("/:id", controller.getSchool);
+router.post("/", controller.createSchool);
+router.put("/:id", controller.updateSchool);
+router.patch("/:id/status", controller.setSchoolStatus);
+
+module.exports = router;
