b5ef313fbd041c18080a63df59f929ea64b492ed
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-01 08:45:53 -0700

    Allow Super Admin to manage selected school class subjects

--- server/routes/classSubjectRoutes.js
+++ server/routes/classSubjectRoutes.js
@@ -1,42 +1,20 @@
 const express = require("express");
-
 const router = express.Router();
-
 const authenticate = require("../middlewares/authenticate");
 const authorize = require("../middlewares/authorize");
 const validate = require("../middlewares/validate");
-
 const ROLE_NAMES = require("../config/roleNames");
-
-const classSubjectController =
-require("../controllers/classSubjectController");
-
-const {
-    createClassSubjectsSchema
-} = require("../validators/classSubjectValidator");
+const classSubjectController = require("../controllers/classSubjectController");
+const { createClassSubjectsSchema } = require("../validators/classSubjectValidator");
 
 router.post(
-
     "/",
-
     authenticate,
-
-    authorize(ROLE_NAMES.ADMIN),
-
+    authorize(ROLE_NAMES.ADMIN, ROLE_NAMES.SUPER_ADMIN),
     validate(createClassSubjectsSchema),
-
     classSubjectController.saveClassSubjects
-
 );
 
-router.get(
-
-    "/:classId",
-
-    authenticate,
-
-    classSubjectController.getClassSubjects
-
-);
+router.get("/:classId", authenticate, classSubjectController.getClassSubjects);
 
 module.exports = router;
