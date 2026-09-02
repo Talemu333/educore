1eb8b0eb70bb6b028be02d5536bb387de745c52e
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-02 06:33:42 -0700

    Load school settings from the public school slug

--- educore-frontend/src/api/schoolSettingsApi.js
+++ educore-frontend/src/api/schoolSettingsApi.js
@@ -1,26 +1,17 @@
 import api from "./axios";
 
+const publicSchoolParams = () => ({
+    schoolSlug: window.location.pathname.split("/").filter(Boolean)[0] || ""
+});
 
 export const getSchoolSettings = async () => {
-
-    const response =
-        await api.get("/school-settings");
-
+    const response = await api.get("/school-settings", {
+        params: publicSchoolParams()
+    });
     return response.data.data;
-
 };
 
-
-export const updateSchoolSettings = async (
-    data
-) => {
-
-    const response =
-        await api.put(
-            "/school-settings",
-            data
-        );
-
+export const updateSchoolSettings = async (data) => {
+    const response = await api.put("/school-settings", data);
     return response.data.data;
-
 };
