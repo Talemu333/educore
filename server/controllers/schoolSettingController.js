0bd0f08bb166e0155137ddd3f1665b8878ef3de9
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-02 06:25:15 -0700

    Allow public school settings lookup by slug

--- server/controllers/schoolSettingController.js
+++ server/controllers/schoolSettingController.js
@@ -1,8 +1,19 @@
 const schoolSettingsService = require("../services/schoolSettingService");
+const schoolSettingsModel = require("../models/schoolSettingModel");
 const asyncHandler = require("../middlewares/asyncHandler");
 
 const getSchoolSettings = asyncHandler(async (req, res) => {
-    const settings = await schoolSettingsService.getSchoolSettings(req.user.school_id);
+    const settings = req.query.schoolSlug
+        ? await schoolSettingsModel.getSchoolSettingsBySlug(req.query.schoolSlug)
+        : await schoolSettingsService.getSchoolSettings(req.user.school_id);
+
+    if (!settings) {
+        return res.status(404).json({
+            success: false,
+            message: "School settings not found."
+        });
+    }
+
     res.json({ success: true, data: settings });
 });
 
