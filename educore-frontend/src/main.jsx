32fe90d7167938d316a2ebec20c1e8736a7e1146
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-03 23:36:31 -0700

    Enable pagination for dashboard financial tables

--- educore-frontend/src/main.jsx
+++ educore-frontend/src/main.jsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import DashboardTablePaginationEnhancer from "./components/common/DashboardTablePaginationEnhancer";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <AuthProvider>
 
             <App />
+
+            <DashboardTablePaginationEnhancer />
 
             <Toaster
                 position="top-right"
