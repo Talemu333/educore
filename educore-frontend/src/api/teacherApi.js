c4844a3a20c8ca9d7453ba7e7a961b9b5348ef43
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-03 21:25:53 -0700

    Support paginated teacher API queries

--- educore-frontend/src/api/teacherApi.js
+++ educore-frontend/src/api/teacherApi.js
@@ -1,71 +1,46 @@
 import api from "./axios";
 
-export const getTeachers = async () => {
+export const getTeachers = async ({
+    search = "",
+    departmentId = "",
+    status = "",
+    page = 1,
+    limit = 10
+} = {}) => {
+    const response = await api.get("/teachers", {
+        params: {
+            search,
+            department_id: departmentId,
+            status,
+            page,
+            limit
+        }
+    });
 
-    const response = await api.get("/teachers");
-
-    return response.data.data;
-
+    return response.data;
 };
 
 export const getTeacher = async (id) => {
-
     const response = await api.get(`/teachers/${id}`);
-
     return response.data.data;
-
 };
 
 export const createTeacher = async (teacherData) => {
-
-    const response = await api.post(
-
-        "/teachers",
-
-        teacherData
-
-    );
-
+    const response = await api.post("/teachers", teacherData);
     return response.data;
-
 };
 
-export const updateTeacher = async (
-
-    id,
-
-    teacherData
-
-) => {
-
-    const response = await api.put(
-
-        `/teachers/${id}`,
-
-        teacherData
-
-    );
-
+export const updateTeacher = async (id, teacherData) => {
+    const response = await api.put(`/teachers/${id}`, teacherData);
     return response.data;
-
 };
 
 export const deactivateTeacher = async (id) => {
-
-    const response = await api.delete(
-
-        `/teachers/${id}`
-
-    );
-
+    const response = await api.delete(`/teachers/${id}`);
     return response.data;
-
 };
 
 export const getTeacherById = async (id) => {
-
     const response = await api.get(`/teachers/${id}`);
-
     return response.data.data;
-
 };
