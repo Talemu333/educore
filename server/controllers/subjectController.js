b79ca178713dedb372fc248a1ce65d91a909f739
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-05 13:00:18 -0700

    Add student subject access

--- server/controllers/subjectController.js
+++ server/controllers/subjectController.js
@@ -1,5 +1,8 @@
 const subjectService = require("../services/subjectService");
+const classSubjectService = require("../services/classSubjectService");
+const studentModel = require("../models/studentModel");
 const asyncHandler = require("../middlewares/asyncHandler");
+const ApiError = require("../utils/ApiError");
 
 const getSubjects = asyncHandler(async (req, res) => {
     const subjects = await subjectService.getSubjects(req.user.school_id);
@@ -7,27 +10,34 @@ const getSubjects = asyncHandler(async (req, res) => {
 });
 
 const createSubject = asyncHandler(async (req, res) => {
-    const subject = await subjectService.createSubject(
-        req.body,
-        req.user.school_id
-    );
-    res.status(201).json({
-        success: true,
-        message: "Subject created successfully.",
-        data: subject
-    });
+    const subject = await subjectService.createSubject(req.body, req.user.school_id);
+    res.status(201).json({ success: true, message: "Subject created successfully.", data: subject });
 });
 
 const getSubjectsByClass = asyncHandler(async (req, res) => {
-    const subjects = await subjectService.getSubjectsByClass(
-        req.params.classId,
-        req.user.school_id
-    );
+    const subjects = await subjectService.getSubjectsByClass(req.params.classId, req.user.school_id);
     res.json({ success: true, data: subjects });
 });
 
-module.exports = {
-    getSubjects,
-    createSubject,
-    getSubjectsByClass
-};
+const getMySubjects = asyncHandler(async (req, res) => {
+    const student = await studentModel.getStudentByUserId(req.user.id, req.user.school_id);
+    if (!student) throw new ApiError(404, "Student profile not found.");
+    const subjects = await classSubjectService.getClassSubjects(student.class_id, req.user.school_id);
+    res.json({
+        success: true,
+        data: {
+            student: {
+                id: student.id,
+                admission_number: student.admission_number,
+                name: [student.surname, student.first_name, student.middle_name].filter(Boolean).join(" "),
+                class_id: student.class_id,
+                class_name: student.class_name,
+                arm_id: student.arm_id,
+                arm_name: student.arm_name
+            },
+            subjects
+        }
+    });
+});
+
+module.exports = { getSubjects, createSubject, getSubjectsByClass, getMySubjects };
