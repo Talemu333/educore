99eabcab3c9d19c7b27e46a4f898697ae78e5807
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 13:53:02 -0700

    Add school database context to notification routes

--- server/routes/notificationRoutes.js
+++ server/routes/notificationRoutes.js
@@ -2,29 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
-const authenticate =
-require("../middlewares/authenticate");
-
-const notificationController =
-require("../controllers/notificationController");
+const authenticate = require("../middlewares/authenticate");
+const schoolDatabaseContext = require("../middlewares/schoolDatabaseContext");
+const notificationController = require("../controllers/notificationController");
 
 router.get(
-
     "/",
-
     authenticate,
-
+    schoolDatabaseContext,
     notificationController.getMyNotifications
-
 );
+
 router.patch(
-
     "/:id/read",
-
     authenticate,
-
+    schoolDatabaseContext,
     notificationController.markAsRead
-
 );
 
 module.exports = router;
