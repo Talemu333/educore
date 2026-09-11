a10ac876dd4f03fc44280e56bbe92e438c7ef503
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 06:03:24 -0700

    Mount EduProw partner routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const router = express.Router();
 
 const generalRoutes = require("./generalRoutes");
+const partnerRoutes = require("./partnerRoutes");
 
 router.use("/", generalRoutes);
+router.use("/api/partners", partnerRoutes);
 
 module.exports = router;
