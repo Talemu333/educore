326f9d2c0c489ebabfc4c52f4d1332800de09d47
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 14:49:58 -0700

    Enable school database context for classes

--- server/routes/classRoutes.js
+++ server/routes/classRoutes.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const authorize = require("../middlewares/authorize");
+const schoolDatabaseContext = require("../middlewares/schoolDatabaseContext");
 const ROLES = require("../constants/roles");
 const classController = require("../controllers/classController");
 
-router.get("/", authenticate, classController.getClasses);
-router.get("/:id/arms", authenticate, classController.getClassArms);
+router.get("/", authenticate, schoolDatabaseContext, classController.getClasses);
+router.get("/:id/arms", authenticate, schoolDatabaseContext, classController.getClassArms);
 router.post(
     "/",
     authenticate,
+    schoolDatabaseContext,
     authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN),
     classController.createClass
 );
