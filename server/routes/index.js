c59e63917e1715ce2309a2144a151f20e13d5449
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 06:39:00 -0700

    Mount Super Admin partner management routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,8 +4,10 @@ const router = express.Router();
 
 const generalRoutes = require("./generalRoutes");
 const partnerRoutes = require("./partnerRoutes");
+const partnerAdminRoutes = require("./partnerAdminRoutes");
 
 router.use("/", generalRoutes);
 router.use("/api/partners", partnerRoutes);
+router.use("/api/partner-admin", partnerAdminRoutes);
 
 module.exports = router;
