585f90e6917b4e39a613fa69f24239d6ed32d76c
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 13:52:52 -0700

    Pass school database context to notification service

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -4,7 +4,8 @@ const asyncHandler = require("../middlewares/asyncHandler");
 const getMyNotifications = asyncHandler(async (req, res) => {
     const notifications = await notificationService.getUserNotifications(
         req.user.id,
-        req.user.school_id
+        req.user.school_id,
+        req.schoolDatabase
     );
     res.json({ success: true, data: notifications });
 });
@@ -12,7 +13,8 @@ const getMyNotifications = asyncHandler(async (req, res) => {
 const markAsRead = asyncHandler(async (req, res) => {
     const notification = await notificationService.markAsRead(
         req.params.id,
-        req.user.school_id
+        req.user.school_id,
+        req.schoolDatabase
     );
     res.json({ success: true, data: notification });
 });
