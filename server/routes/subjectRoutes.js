1d46e64d266118b37868c597809a596d72f5535d
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 15:06:01 -0700

    Resolve school database for subject routes

--- server/routes/subjectRoutes.js
+++ server/routes/subjectRoutes.js
@@ -3,13 +3,14 @@ const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const subjectController = require("../controllers/subjectController");
 const authorize = require("../middlewares/authorize");
+const schoolDatabaseContext = require("../middlewares/schoolDatabaseContext");
 const ROLES = require("../constants/roles");
 const validate = require("../middlewares/validate");
 const { createSubjectSchema } = require("../validators/subjectValidator");
 
-router.get("/", authenticate, subjectController.getSubjects);
-router.get("/my", authenticate, authorize(ROLES.STUDENT), subjectController.getMySubjects);
-router.get("/class/:classId", authenticate, subjectController.getSubjectsByClass);
-router.post("/", authenticate, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), validate(createSubjectSchema), subjectController.createSubject);
+router.get("/", authenticate, schoolDatabaseContext, subjectController.getSubjects);
+router.get("/my", authenticate, schoolDatabaseContext, authorize(ROLES.STUDENT), subjectController.getMySubjects);
+router.get("/class/:classId", authenticate, schoolDatabaseContext, subjectController.getSubjectsByClass);
+router.post("/", authenticate, schoolDatabaseContext, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), validate(createSubjectSchema), subjectController.createSubject);
 
 module.exports = router;
