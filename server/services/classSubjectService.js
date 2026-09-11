2ce9a1737c8beb34cdd024833ba8f0298c5f1147
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 14:53:34 -0700

    Use resolved school database for class subjects

--- server/services/classSubjectService.js
+++ server/services/classSubjectService.js
@@ -1,11 +1,18 @@
-const pool = require("../config/database");
 const ApiError = require("../utils/ApiError");
 const classModel = require("../models/classModel");
 const subjectModel = require("../models/subjectModel");
 const classSubjectModel = require("../models/classSubjectModel");
 
-const saveClassSubjects = async (data, schoolId) => {
-    const schoolClass = await classModel.getClassById(data.class_id, schoolId);
+const saveClassSubjects = async (data, schoolId, database) => {
+    if (!schoolId) {
+        throw new ApiError(403, "School context is required.");
+    }
+
+    if (!database) {
+        throw new ApiError(500, "School database context is unavailable.");
+    }
+
+    const schoolClass = await classModel.getClassById(data.class_id, schoolId, database);
 
     if (!schoolClass) {
         throw new ApiError(404, "Class not found in this school.");
@@ -14,7 +21,8 @@ const saveClassSubjects = async (data, schoolId) => {
     for (const subject of data.subjects) {
         const existingSubject = await subjectModel.getSubjectById(
             subject.subject_id,
-            schoolId
+            schoolId,
+            database
         );
 
         if (!existingSubject) {
@@ -25,7 +33,7 @@ const saveClassSubjects = async (data, schoolId) => {
         }
     }
 
-    const client = await pool.connect();
+    const client = await database.connect();
 
     try {
         await client.query("BEGIN");
@@ -33,11 +41,15 @@ const saveClassSubjects = async (data, schoolId) => {
         await classSubjectModel.deleteByClassId(client, data.class_id, schoolId);
 
         for (const subject of data.subjects) {
-            await classSubjectModel.create(client, {
-                class_id: data.class_id,
-                subject_id: subject.subject_id,
-                is_compulsory: subject.is_compulsory
-            }, schoolId);
+            await classSubjectModel.create(
+                client,
+                {
+                    class_id: data.class_id,
+                    subject_id: subject.subject_id,
+                    is_compulsory: subject.is_compulsory
+                },
+                schoolId
+            );
         }
 
         await client.query("COMMIT");
@@ -48,17 +60,25 @@ const saveClassSubjects = async (data, schoolId) => {
         client.release();
     }
 
-    return await classSubjectModel.getByClassId(data.class_id, schoolId);
+    return await classSubjectModel.getByClassId(data.class_id, schoolId, database);
 };
 
-const getClassSubjects = async (classId, schoolId) => {
-    const schoolClass = await classModel.getClassById(classId, schoolId);
+const getClassSubjects = async (classId, schoolId, database) => {
+    if (!schoolId) {
+        throw new ApiError(403, "School context is required.");
+    }
+
+    if (!database) {
+        throw new ApiError(500, "School database context is unavailable.");
+    }
+
+    const schoolClass = await classModel.getClassById(classId, schoolId, database);
 
     if (!schoolClass) {
         throw new ApiError(404, "Class not found in this school.");
     }
 
-    return await classSubjectModel.getByClassId(classId, schoolId);
+    return await classSubjectModel.getByClassId(classId, schoolId, database);
 };
 
 module.exports = {
