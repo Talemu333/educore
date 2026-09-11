8892e5d473bb2b32bbcc67a2de9f3bab29b8b625
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 14:50:07 -0700

    Enable school database context for class arms

--- server/routes/armRoutes.js
+++ server/routes/armRoutes.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 const authenticate = require("../middlewares/authenticate");
 const authorize = require("../middlewares/authorize");
+const schoolDatabaseContext = require("../middlewares/schoolDatabaseContext");
 const ROLES = require("../constants/roles");
 const armController = require("../controllers/armController");
 
-router.get("/", authenticate, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.getArms);
-router.get("/class/:classId", authenticate, armController.getArmsByClass);
-router.post("/", authenticate, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.createArm);
+router.get("/", authenticate, schoolDatabaseContext, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.getArms);
+router.get("/class/:classId", authenticate, schoolDatabaseContext, armController.getArmsByClass);
+router.post("/", authenticate, schoolDatabaseContext, authorize(ROLES.ADMIN, ROLES.SUPER_ADMIN), armController.createArm);
 
 module.exports = router;
