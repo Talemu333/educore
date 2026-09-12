0d8c70c43d6d639f62a96dc6ef1e07efcf39b93d
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-11 18:15:43 -0700

    Route database queries through school request context

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,25 @@
 const { Pool } = require("pg");
 const { getDatabaseConfig } = require("./databaseConfig");
+const { getSchoolDatabaseFromContext } = require("./databaseContext");
 
-const pool = new Pool(getDatabaseConfig());
+// This pool is the central/platform database pool.
+const centralPool = new Pool(getDatabaseConfig());
 
-pool.on("error", (error) => {
-    console.error("Unexpected PostgreSQL pool error:", error);
+centralPool.on("error", (error) => {
+    console.error("Unexpected central PostgreSQL pool error:", error);
 });
 
-module.exports = pool;
+const activePool = () => getSchoolDatabaseFromContext() || centralPool;
+
+// Existing models can continue using this module. During a school request,
+// their queries are transparently routed to that school's database.
+const database = {
+    query: (...args) => activePool().query(...args),
+    connect: (...args) => activePool().connect(...args),
+    end: (...args) => activePool().end(...args),
+    on: (...args) => activePool().on(...args),
+};
+
+database.centralPool = centralPool;
+
+module.exports = database;
