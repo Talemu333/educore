07b2db92ff3e048139881291e4b16ab8f1faaadf
Talemu333 <82183012+Talemu333@users.noreply.github.com>
2026-09-17 00:42:22 -0700

    refactor: remove duplicate change password page

--- educore-frontend/src/pages/auth/ChangePasswordPage.jsx
+++ educore-frontend/src/pages/auth/ChangePasswordPage.jsx
@@ -1,11 +1,1 @@
-function ChangePasswordPage() {
-    return (
-        <div className="p-10">
-            <h1 className="text-3xl font-bold">
-                Change Password
-            </h1>
-        </div>
-    );
-}
-
-export default ChangePasswordPage;
+export { default } from "../ChangePasswordPage";
